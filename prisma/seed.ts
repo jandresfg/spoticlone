ac7cc4aa972204ab108e8fb4776c49e270d9179b
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-10 08:32:21 -0500

    feat: prisma seed script using songsData

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { artistsData } from "./songsData";
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((s) => ({
+              name: s.name,
+              duration: s.duration,
+              url: s.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
