5acdee918a194c36397a299afef9f2272c285483
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-11 14:05:46 -0500

    feat: order playlist by name

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -4,6 +4,7 @@ import { validateRoute } from "../../lib/auth";
 export default validateRoute(async (req, res, user) => {
   const playlists = await prisma.playlist.findMany({
     where: { userId: user.id },
+    orderBy: { name: "asc" },
   });
 
   res.json(playlists);
