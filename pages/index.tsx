50765acdac792eb1fd6f65f69e42ff28f355bb67
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-11 18:21:48 -0500

    feat: artists section in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       color="red"
@@ -10,9 +13,38 @@ const Home = () => {
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png"
       roundImage
     >
-      home
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="medium">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return { props: { artists } };
 };
 
 export default Home;
