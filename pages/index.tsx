5659c869d47c27c731133143341dd814bf4dc34b
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-11 17:06:33 -0500

    feat: created and used gradientLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      title="Andres Fernandez"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png"
+      roundImage
+    >
+      home
+    </GradientLayout>
+  );
 };
 
 export default Home;
