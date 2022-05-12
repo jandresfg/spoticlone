5b56b80c6057fa676d912fa81bc2da2f0213e14a
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-11 22:33:00 -0500

    feat:use gradientLayout in playlistpage

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,9 +1,37 @@
+import { Text } from "@chakra-ui/layout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
+import GradientLayout from "../../components/gradientLayout";
 
-const Playlist = ({ playlist }) => (
-  <div>Playlist with id {`${playlist.id}`}</div>
-);
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "orange",
+    "gray",
+    "purple",
+    "teal",
+  ];
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+      roundImage={false}
+    >
+      <Text color="white">{JSON.stringify(playlist, null, 3)}</Text>
+    </GradientLayout>
+  );
+};
 
 export const getServerSideProps = async ({ query, req }) => {
   const user = validateToken(req.cookies.SPOTICLONE_ACCESS_TOKEN);
